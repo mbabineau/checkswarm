cb3d79677aa2b5ac32d4c08318811ad056e0d1d5
Mike Babineau <michael.babineau@gmail.com>
2014-09-04 17:51:18 -0700

    init

---
+++ check_executor.py
@@ -0,0 +1,126 @@
+#!/usr/bin/env python
+import re
+import socket
+import subprocess
+import sys
+import threading
+import time
+
+try:
+    from mesos.native import MesosExecutorDriver, MesosSchedulerDriver
+    from mesos.interface import Executor, Scheduler
+    from mesos.interface import mesos_pb2
+except ImportError:
+    from mesos import Executor, MesosExecutorDriver, MesosSchedulerDriver, Scheduler
+    import mesos_pb2
+
+
+class CheckExecutor(Executor):
+    # Replaces invalid characters for use in Graphite
+    CARBON_REPLACEMENT_CHARACTER = '_'
+
+    def __init__(self, carbon_host='internal-test3a-gr-PrivateE-12D2MV3JAIIB5-1753911466.us-west-2.elb.amazonaws.com', carbon_port=2003):
+        self.carbon_host = carbon_host
+        self.carbon_port = carbon_port
+        self.carbon_queue = []
+
+    def _send_to_graphite(self, metric_path, value, ts=None):
+        if not ts:
+            ts = int(time.time())
+        message = '%s %s %s\n' % (metric_path, value, ts)
+        sock = socket.socket()
+        sock.connect((self.carbon_host, self.carbon_port))
+        sock.sendall(message)
+        sock.close()
+
+    def _extract_perfdata(self, raw_perfdata):
+        """Parses Nagios check performance data and returns a list of (label, value) tuples"""
+        # Adapted from https://github.com/shawn-sterling/graphios/blob/master/graphios.py
+        matches = re.finditer(r'(?P<perfdata>(?P<label>.*?)=(?P<value>[-0-9\.]+)\S*\s?)', raw_perfdata)
+        parsed_perfdata = [match.groupdict() for match in matches]
+        return [
+            (re.sub(r'[\s\.:\\/]', self.CARBON_REPLACEMENT_CHARACTER, p['label']), p['value'])
+            for p in parsed_perfdata
+        ]
+
+    def _handle_result(self, check_command, args, return_code, output):
+        print '%s returned with code [%s] and message: %s' % (check_command, return_code, output)
+
+        raw_perfdata = output.split('|')[1]
+        perfdata = self._extract_perfdata(raw_perfdata)
+
+        host = re.sub(r'[\s\.:\\]', self.CARBON_REPLACEMENT_CHARACTER, args[args.index('-H')+1]) if '-H' in args else 'local'
+
+        metric_prefix = '.'.join([check_command, host])
+        if check_command == 'check_http':
+            url = re.sub(r'[\s\.:\\\/\()]', self.CARBON_REPLACEMENT_CHARACTER, args[args.index('-u')+1]) if '-u' in args else '_'
+            metric_prefix = '.'.join([metric_prefix, url])
+
+        # Send check result and perf data to Graphite
+        self._send_to_graphite('.'.join([metric_prefix, '_status']), return_code)
+        for label, value in perfdata:
+            self._send_to_graphite('.'.join([metric_prefix, label]), value)
+
+    def _parse_task_data(self, task):
+        args = task.data.split(' ')
+        check_command = args.pop(0)
+        return check_command, args
+
+    def registered(self, driver, executorInfo, frameworkInfo, slaveInfo):
+        print 'CheckExecutor registered'
+
+    def reregistered(self, driver, slaveInfo):
+        print 'CheckExecutor reregistered'
+
+    def disconnected(self, driver):
+        print 'CheckExecutor disconnected'
+
+    def launchTask(self, driver, task):
+        def run_task():
+            print 'Running check task %s' % task.task_id.value
+            update = mesos_pb2.TaskStatus()
+            update.task_id.value = task.task_id.value
+            update.state = mesos_pb2.TASK_RUNNING
+            driver.sendStatusUpdate(update)
+
+            check_command, args = self._parse_task_data(task)
+
+            try:
+                output = subprocess.check_output(['docker', 'run', '--rm', 'thefactory/nagios-plugins', check_command]+args)
+                return_code = 0
+            except subprocess.CalledProcessError, e:
+                output = e.output
+                return_code = e.returncode
+
+            self._handle_result(check_command, args, return_code, output)
+
+            print 'Sending status update for task %s' % task.task_id.value
+            update = mesos_pb2.TaskStatus()
+            update.task_id.value = task.task_id.value
+            update.state = mesos_pb2.TASK_FINISHED
+            update.message = output
+            driver.sendStatusUpdate(update)
+            print 'Sent status update for task %s' % task.task_id.value
+            return
+
+        thread = threading.Thread(target=run_task)
+        thread.start()
+
+    def killTask(self, driver, taskId):
+        self.shutdown(self, driver)
+
+    def frameworkMessage(self, driver, message):
+        pass
+
+    def shutdown(self, driver):
+        print "Shutting down"
+        sys.exit(0)
+
+    def error(self, error, message):
+        pass
+
+if __name__ == '__main__':
+    print 'Starting CheckExecutor'
+
+    driver = MesosExecutorDriver(CheckExecutor())
+    sys.exit(0 if driver.run() == mesos_pb2.DRIVER_STOPPED else 1)
---
+++ checkswarm.py
@@ -0,0 +1,192 @@
+#!/usr/bin/env python
+import json
+import signal
+import socket
+import sys
+import time
+from threading import Thread
+
+try:
+    from mesos.native import MesosExecutorDriver, MesosSchedulerDriver
+    from mesos.interface import Executor, Scheduler
+    from mesos.interface import mesos_pb2
+except ImportError:
+    from mesos import Executor, MesosExecutorDriver, MesosSchedulerDriver, Scheduler
+    import mesos_pb2
+
+
+TASK_CPUS = 0.1
+TASK_MEM = 8
+SHUTDOWN_TIMEOUT = 30  # in seconds
+LEADING_ZEROS_COUNT = 5  # appended to task ID to facilitate lexicographical order
+TASK_ATTEMPTS = 5  # how many times a task is attempted
+
+CHECK_QUEUE = [
+    'check_http -H www.google.com',
+    'check_http -H www.docker.com -S',
+    'check_http -H mesosphere.io',
+    'check_http -H www.amazon.com',
+    'check_http -H www.thefactory.com',
+    'check_http -H www.github.com',
+    'check_http -H en.wikipedia.org -u /wiki/E._Clarke_and_Julia_Arnold_House',
+    'check_http -H en.wikipedia.org -u /wiki/Hendel_Brothers,_Sons_and_Company_Hat_Factory',
+    'check_http -H en.wikipedia.org -u /wiki/Raedieahkka',
+    'check_http -H en.wikipedia.org -u /wiki/Celtic_music_in_Canada',
+    'check_http -H en.wikipedia.org -u /wiki/Philip_Bermingham',
+    'check_http -H en.wikipedia.org -u /wiki/Fabric_49',
+    'check_http -H en.wikipedia.org -u /wiki/Kahrizak,_Pakdasht',
+    'check_http -H en.wikipedia.org -u /wiki/WOWO_(AM)',
+    'check_load -w 0 -c 3'
+]
+
+TASK_STATES = {
+    6: 'TASK_STAGING',  # Initial state. Framework status updates should not use.
+    0: 'TASK_STARTING',
+    1: 'TASK_RUNNING',
+    2: 'TASK_FINISHED', # TERMINAL.
+    3: 'TASK_FAILED',   # TERMINAL.
+    4: 'TASK_KILLED',   # TERMINAL.
+    5: 'TASK_LOST'      # TERMINAL.
+}
+
+
+class CheckSwarm(Scheduler):
+    def __init__(self):
+        print 'checkswarm init'
+        self.tasks_created = 0
+        self.timers = {}
+
+        self.index = 0 # for testing only
+
+    def _calc_max_tasks(self, offer):
+        count = 0
+        cpus = next(rsc.scalar.value for rsc in offer.resources if rsc.name == 'cpus')
+        mem = next(rsc.scalar.value for rsc in offer.resources if rsc.name == 'mem')
+        while cpus >= TASK_CPUS and mem >= TASK_MEM:
+            count += 1
+            cpus -= TASK_CPUS
+            mem -= TASK_MEM
+        return count
+
+    def _build_task(self, offer, check):
+        task = mesos_pb2.TaskInfo()
+
+        tid = self.tasks_created
+        self.tasks_created += 1
+        task.task_id.value = str(tid).zfill(LEADING_ZEROS_COUNT)
+        task.slave_id.value = offer.slave_id.value
+
+        cpus = task.resources.add()
+        cpus.name = 'cpus'
+        cpus.type = mesos_pb2.Value.SCALAR
+        cpus.scalar.value = TASK_CPUS
+
+        mem = task.resources.add()
+        mem.name = 'mem'
+        mem.type = mesos_pb2.Value.SCALAR
+        mem.scalar.value = TASK_MEM
+
+        task.name = 'check %s' % task.task_id.value
+
+        container = mesos_pb2.ContainerInfo()
+        container.type = mesos_pb2.ContainerInfo.DOCKER
+        docker = mesos_pb2.ContainerInfo.DockerInfo()
+        docker.image = 'thefactory/nagios-plugins'
+        container.docker.CopyFrom(docker)
+        task.container.CopyFrom(container)
+
+        task.data = check
+
+        # command = mesos_pb2.CommandInfo()
+        # command.shell = False
+        # command.arguments.extend(check.split(' '))
+        # task.command.CopyFrom(command)
+
+        executor = mesos_pb2.ExecutorInfo()
+        # executor.executor_id.value = '-'.join(['check_executor',task.task_id.value])
+        executor.executor_id.value = 'check_executor'
+        executor.command.value = 'python /tmp/check_executor.py'
+        task.executor.MergeFrom(executor)
+        return task
+
+    def registered(self, driver, frameworkId, masterInfo):
+        print 'Registered with framework ID [%s]' % frameworkId.value
+
+    def reregistered(self, driver, frameworkId, masterInfo):
+        print 'Reregistered with framework ID [%s]' % frameworkId.value
+
+    def disconnected(self, driver, frameworkId, masterInfo):
+        print 'Disconnected'
+
+    def resourceOffers(self, driver, offers):
+        for offer in offers:
+            print 'Got resource offer [%s]' % offer.id.value
+
+            tasks = []
+            if CHECK_QUEUE:
+                for i in range(self._calc_max_tasks(offer)):
+                    tasks.append(self._build_task(offer, CHECK_QUEUE[self.index]))
+
+                    self.index += 1
+                    if self.index == len(CHECK_QUEUE): self.index = 0
+
+                    # tasks.append(self._build_task(offer, CHECK_QUEUE.pop()))
+
+            if tasks:
+                print 'Accepting offer on [%s]' % offer.hostname
+                driver.launchTasks(offer.id, tasks)
+                [self.timers.update({ task.task_id.value: time.time()}) for task in tasks]
+            else:
+                print 'Declining offer on [%s]' % offer.hostname
+                driver.declineOffer(offer.id)
+
+    def statusUpdate(self, driver, update):
+        state_name = TASK_STATES[update.state]
+        print 'Task [%s] is in state [%s] with message: %s' % (update.task_id.value, state_name, update.message)
+        if state_name in ['TASK_FINISHED', 'TASK_FAILED', 'TASK_KILLED', 'TASK_LOST']:
+            end_time = time.time()
+            start_time = self.timers.pop(update.task_id.value, end_time)
+            print 'Task %s completed in %s seconds' % (update.task_id.value, end_time-start_time)
+
+            time_in_ms = int(1000*(end_time-start_time))
+            udp_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+            udp_sock.sendto(
+                ''.join(['checkswarm:', str(time_in_ms), '|ms']).encode('utf-8'),
+                ('ec2-54-68-89-164.us-west-2.compute.amazonaws.com', 8125)
+            )
+
+
+    def frameworkMessage(self, driver, executorId, slaveId, message):
+        o = json.loads(message)
+        print 'Got framework message: %s' % o
+
+
+def shutdown(signal, frame):
+    driver.stop()
+
+
+if __name__ == '__main__':
+    framework = mesos_pb2.FrameworkInfo()
+    framework.user = '' # Have Mesos fill in the current user.
+    framework.name = 'checkswarm'
+    framework.failover_timeout = 60
+
+    checkswarm = CheckSwarm()
+
+    driver = MesosSchedulerDriver(checkswarm, framework, sys.argv[1])
+
+    # driver.run() blocks; we run it in a separate thread
+    def run_driver_async():
+        status = 0 if driver.run() == mesos_pb2.DRIVER_STOPPED else 1
+        driver.stop()
+        sys.exit(status)
+    framework_thread = Thread(target = run_driver_async, args = ())
+    framework_thread.start()
+
+    print '(Listening for Ctrl-C)'
+    signal.signal(signal.SIGINT, shutdown)
+    while framework_thread.is_alive():
+        time.sleep(1)
+
+    print 'Goodbye!'
+    sys.exit(0)
